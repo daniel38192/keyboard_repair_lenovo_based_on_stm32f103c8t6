df18a20b8217b16d81e35d96bb6356cdf9fc9492
daniel38192 <71901962+daniel38192@users.noreply.github.com>
2025-05-27 21:27:39 -0600

    Delete cmake-build-debug directory
    delete directory

--- cmake-build-debug/CMakeFiles/clean_additional.cmake
+++
@@ -1,8 +0,0 @@
-# Additional clean files
-cmake_minimum_required(VERSION 3.16)
-
-if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
-  file(REMOVE_RECURSE
-  "keyboard_repair_lenovo_based_on_stm32f103c8t6.map"
-  )
-endif()
